e215d374b1966eab45d4fa7d7bf6253dd5a50a49
Kim Nilsson <kim@wayoftao.net>
2022-01-23 22:22:18 +0100

    Add AVP impl

--- src/avp/types.rs
+++ src/avp/types.rs
@@ -1,28 +1,197 @@
+#[derive(Clone, Debug, PartialEq)]
 pub struct MessageType {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct ResultCode {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct ProtocolVersion {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct FramingCapabilities {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct BearerCapabilities {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct TieBreaker {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct FirmwareRevision {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct HostName {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct VendorName {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct AssignedTunnelId {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct ReceiveWindowSize {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct Challenge {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct ChallengeResponse {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct Q931CauseCode {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct AssignedSessionId {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct CallSerialNumber {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct MinimumBps {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct MaximumBps {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct BearerType {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct FramingType {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct CalledNumber {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct CallingNumber {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct SubAddress {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct TxConnectSpeed {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct RxConnectSpeed {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct PhysicalChannelId {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct PrivateGroupId {}
+#[derive(Clone, Debug, PartialEq)]
 pub struct SequencingRequired {}
+
+impl MessageType {
+    pub fn from(_data: &[u8]) -> Self {
+        Self {}
+    }
+}
+impl ResultCode {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl ProtocolVersion {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl FramingCapabilities {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl BearerCapabilities {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl TieBreaker {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl FirmwareRevision {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl HostName {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl VendorName {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl AssignedTunnelId {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl ReceiveWindowSize {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl Challenge {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl ChallengeResponse {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl Q931CauseCode {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl AssignedSessionId {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl CallSerialNumber {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl MinimumBps {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl MaximumBps {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl BearerType {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl FramingType {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl CalledNumber {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl CallingNumber {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl SubAddress {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl TxConnectSpeed {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl RxConnectSpeed {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl PhysicalChannelId {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl PrivateGroupId {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
+impl SequencingRequired {
+    pub fn from(_data: &[u8]) -> Self {
+        unimplemented!();
+    }
+}
--- src/message/tests.rs
+++ src/message/tests.rs
@@ -1,90 +1,98 @@
 use crate::message::*;
 
 #[test]
-fn from_bytes() {
+fn from_bytes_data_valid() {
+    // Data message
+    let input = vec![0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0xff];
+    let m = Message::from_bytes(
+        &input,
+        ValidateReserved::No,
+        ValidateVersion::Yes,
+        ValidateUnused::No,
+    );
+    assert_eq!(
+        m,
+        Ok(Message::Data(DataMessage {
+            length: None,
+            tunnel_id: 0,
+            session_id: 0,
+            ns_nr: None,
+            offset: None,
+            data: &input[6..]
+        }))
+    );
+}
+
+#[test]
+fn from_bytes_data_invalid_version() {
     // Data message with invalid version
-    {
-        let input = vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff];
-        let m = Message::from_bytes(
-            &input,
-            ValidateReserved::No,
-            ValidateVersion::Yes,
-            ValidateUnused::No,
-        );
-        assert_eq!(m, Err("Invalid version encountered"));
-    }
-    // Data message with valid version
-    {
-        let input = vec![0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0xff];
-        let m = Message::from_bytes(
-            &input,
-            ValidateReserved::No,
-            ValidateVersion::Yes,
-            ValidateUnused::No,
-        );
-        assert_eq!(
-            m,
-            Ok(Message::Data(DataMessage {
-                length: None,
-                tunnel_id: 0,
-                session_id: 0,
-                ns_nr: None,
-                offset: None,
-                data: &input[6..]
-            }))
-        );
-    }
+    let input = vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff];
+    let m = Message::from_bytes(
+        &input,
+        ValidateReserved::No,
+        ValidateVersion::Yes,
+        ValidateUnused::No,
+    );
+    assert_eq!(m, Err("Invalid version encountered"));
+}
+
+#[test]
+fn from_bytes_control_valid() {
+    // ControlMessage with Message Type AVP
+    use crate::avp::{types, AVP};
+    let input = vec![
+        0x13, 0x00, // Flags
+        0x00, 0x14, // Length
+        0x00, 0x02, // Tunnel ID
+        0x00, 0x03, // Session ID
+        0x00, 0x04, // Ns
+        0x00, 0x05, // Nr
+        // Payload
+        0x00, 0x08, // Flags and Length
+        0x00, 0x00, // Vendor ID
+        0x00, 0x00, // Attribute Type (Message Type)
+        0x00, 0x00, // Type 0
+    ];
+    let m = Message::from_bytes(
+        &input,
+        ValidateReserved::No,
+        ValidateVersion::No,
+        ValidateUnused::Yes,
+    );
+    assert_eq!(
+        m,
+        Ok(Message::Control(ControlMessage {
+            length: 20,
+            tunnel_id: 2,
+            session_id: 3,
+            ns: 4,
+            nr: 5,
+            avps: vec![AVP::MessageType(types::MessageType {})],
+        }))
+    );
+}
+
+#[test]
+fn from_bytes_control_invalid_priority() {
     // ControlMessage with invalid priority
-    {
-        let input = vec![
-            0x83, 0x00, // Flags
-            0x00, 0x04, // Length
-            0x00, 0x02, // Tunnel ID
-            0x00, 0x03, // Session ID
-            0x00, 0x04, // Ns
-            0x00, 0x05, // Nr
-            0xde, 0xad, // Payload
-            0xbe, 0xef,
-        ];
-        let m = Message::from_bytes(
-            &input,
-            ValidateReserved::No,
-            ValidateVersion::No,
-            ValidateUnused::Yes,
-        );
-        assert_eq!(
-            m,
-            Err("Control message with forbidden Priority bit encountered")
-        );
-    }
-    // ControlMessage with valid priority
-    {
-        let input = vec![
-            0x13, 0x00, // Flags
-            0x00, 0x04, // Length
-            0x00, 0x02, // Tunnel ID
-            0x00, 0x03, // Session ID
-            0x00, 0x04, // Ns
-            0x00, 0x05, // Nr
-            0xde, 0xad, // Payload
-            0xbe, 0xef,
-        ];
-        let m = Message::from_bytes(
-            &input,
-            ValidateReserved::No,
-            ValidateVersion::No,
-            ValidateUnused::Yes,
-        );
-        assert_eq!(
-            m,
-            Ok(Message::Control(ControlMessage {
-                length: 4,
-                tunnel_id: 2,
-                session_id: 3,
-                ns: 4,
-                nr: 5,
-                data: &input[12..]
-            }))
-        );
-    }
+    let input = vec![
+        0x83, 0x00, // Flags
+        0x00, 0x04, // Length
+        0x00, 0x02, // Tunnel ID
+        0x00, 0x03, // Session ID
+        0x00, 0x04, // Ns
+        0x00, 0x05, // Nr
+        0xde, 0xad, // Payload
+        0xbe, 0xef,
+    ];
+    let m = Message::from_bytes(
+        &input,
+        ValidateReserved::No,
+        ValidateVersion::No,
+        ValidateUnused::Yes,
+    );
+    assert_eq!(
+        m,
+        Err("Control message with forbidden Priority bit encountered")
+    );
 }
