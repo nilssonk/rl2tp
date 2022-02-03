5cdeffbbd02576e8c3753c16a16fb328ab6831c0
Kim Nilsson <92694405+nilssonk@users.noreply.github.com>
2022-02-03 18:56:44 +0100

    Add and implement RandomVector AVP (#19)

--- src/message/tests/valid_avp.rs
+++ src/message/tests/valid_avp.rs
@@ -1,45 +1,7 @@
 use crate::message::*;
 
-mod valid_avp;
-
 #[test]
-fn from_bytes_data_valid() {
-    // Data message
-    let input = vec![0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0xff];
-    let m = Message::from_bytes(
-        &input,
-        ValidateReserved::No,
-        ValidateVersion::Yes,
-        ValidateUnused::No,
-    );
-    assert_eq!(
-        m,
-        Ok(Message::Data(DataMessage {
-            length: None,
-            tunnel_id: 0,
-            session_id: 0,
-            ns_nr: None,
-            offset: None,
-            data: &input[6..]
-        }))
-    );
-}
-
-#[test]
-fn from_bytes_data_invalid_version() {
-    // Data message with invalid version
-    let input = vec![0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff];
-    let m = Message::from_bytes(
-        &input,
-        ValidateReserved::No,
-        ValidateVersion::Yes,
-        ValidateUnused::No,
-    );
-    assert_eq!(m, Err("Invalid version encountered"));
-}
-
-#[test]
-fn from_bytes_control_valid() {
+fn message_type() {
     // ControlMessage with Message Type AVP
     use crate::avp::{types, AVP};
     let input = vec![
@@ -49,7 +11,7 @@ fn from_bytes_control_valid() {
         0x00, 0x03, // Session ID
         0x00, 0x04, // Ns
         0x00, 0x05, // Nr
-        // Payload
+        // AVP Payload
         0x00, 0x08, // Flags and Length
         0x00, 0x00, // Vendor ID
         0x00, 0x00, // Attribute Type (Message Type)
@@ -77,16 +39,26 @@ fn from_bytes_control_valid() {
 }
 
 #[test]
-fn from_bytes_control_invalid_priority() {
-    // ControlMessage with invalid priority
+fn random_vector() {
+    // ControlMessage with Random Vector AVP
+    use crate::avp::{types, AVP};
     let input = vec![
-        0x83, 0x00, // Flags
-        0x00, 0x04, // Length
+        0x13, 0x00, // Flags
+        0x00, 0x1e, // Length
         0x00, 0x02, // Tunnel ID
         0x00, 0x03, // Session ID
         0x00, 0x04, // Ns
         0x00, 0x05, // Nr
-        0xde, 0xad, // Payload
+        // AVP Payload
+        0x00, 0x08, // Flags and Length
+        0x00, 0x00, // Vendor ID
+        0x00, 0x00, // Attribute Type (Message Type)
+        0x00, 0x01, // Type 1 (StartControlConnectionRequest)
+        // AVP Payload
+        0x00, 0x0a, // Flags and Length
+        0x00, 0x00, // Vendor ID
+        0x00, 0x24, // Attribute Type (Random Vector)
+        0xde, 0xad, // Random Vector payload
         0xbe, 0xef,
     ];
     let m = Message::from_bytes(
@@ -97,6 +69,18 @@ fn from_bytes_control_invalid_priority() {
     );
     assert_eq!(
         m,
-        Err("Control message with forbidden Priority bit encountered")
+        Ok(Message::Control(ControlMessage {
+            length: 30,
+            tunnel_id: 2,
+            session_id: 3,
+            ns: 4,
+            nr: 5,
+            avps: vec![
+                AVP::MessageType(types::MessageType::StartControlConnectionRequest),
+                AVP::RandomVector(types::RandomVector {
+                    data: [0xde, 0xad, 0xbe, 0xef]
+                })
+            ],
+        }))
     );
 }
