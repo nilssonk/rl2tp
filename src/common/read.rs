703c4126d3e8cab122b3ee4926af9964b36ef3dc
Kim Nilsson <kim@wayoftao.net>
2022-02-06 00:36:48 +0100

    Add read_u32_be_unchecked

--- src/common/read.rs
+++ src/common/read.rs
@@ -1,3 +1,7 @@
 pub unsafe fn read_u16_be_unchecked(input: &[u8]) -> u16 {
     u16::from_be_bytes(input[..2].try_into().unwrap_unchecked())
 }
+
+pub unsafe fn read_u32_be_unchecked(input: &[u8]) -> u32 {
+    u32::from_be_bytes(input[..4].try_into().unwrap_unchecked())
+}
