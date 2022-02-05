d0ae88398fcc27453d4e0b2b8221028dd45e0e74
Kim Nilsson <92694405+nilssonk@users.noreply.github.com>
2022-02-06 00:03:09 +0100

    Add and implement ProtocolVersion AVP (#24)

---
+++ src/avp/types/protocol_version.rs
@@ -0,0 +1,20 @@
+use crate::common::ResultStr;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct ProtocolVersion {
+    pub version: u8,
+    pub revision: u8,
+}
+
+impl ProtocolVersion {
+    pub fn from(input: &[u8]) -> ResultStr<Self> {
+        if input.len() < 2 {
+            return Err("Incomplete ProtocolVersion AVP encountered");
+        }
+
+        Ok(Self {
+            version: input[0],
+            revision: input[1],
+        })
+    }
+}
