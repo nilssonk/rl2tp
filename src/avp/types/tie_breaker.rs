b0d2008a6d815739927322b0e7840ba3d19737b0
Kim Nilsson <kim@wayoftao.net>
2022-02-06 23:33:08 +0100

    Add and implement TieBreaker AVP

---
+++ src/avp/types/tie_breaker.rs
@@ -0,0 +1,23 @@
+use crate::common::{read_u64_be_unchecked, ResultStr};
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct TieBreaker {
+    pub value: u64,
+}
+
+impl TieBreaker {
+    pub fn from(input: &[u8]) -> ResultStr<Self> {
+        if input.len() < 8 {
+            return Err("Incomplete TieBreaker AVP encountered");
+        }
+
+        let value = unsafe { read_u64_be_unchecked(input) };
+        Ok(Self { value })
+    }
+}
+
+impl From<u64> for TieBreaker {
+    fn from(value: u64) -> Self {
+        Self { value }
+    }
+}
