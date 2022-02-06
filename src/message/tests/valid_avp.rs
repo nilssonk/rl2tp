9f0c5080b70b051b761b8c8204a5a2db029ec8db
Kim Nilsson <kim@wayoftao.net>
2022-02-07 00:03:29 +0100

    Add and implement HostName AVP

--- src/message/tests/valid_avp.rs
+++ src/message/tests/valid_avp.rs
@@ -386,3 +386,50 @@ fn firmware_revision() {
         }))
     );
 }
+
+#[test]
+fn host_name() {
+    // ControlMessage with Host Name AVP
+    use crate::avp::{types, AVP};
+    let input = vec![
+        0x13, 0x00, // Flags
+        0x00, 0x1e, // Length
+        0x00, 0x02, // Tunnel ID
+        0x00, 0x03, // Session ID
+        0x00, 0x04, // Ns
+        0x00, 0x05, // Nr
+        // AVP Payload
+        0x00, 0x08, // Flags and Length
+        0x00, 0x00, // Vendor ID
+        0x00, 0x00, // Attribute Type (Message Type)
+        0x00, 0x01, // Type 1 (StartControlConnectionRequest)
+        // AVP Payload
+        0x00, 0x0a, // Flags and Length
+        0x00, 0x00, // Vendor ID
+        0x00, 0x07, // Attribute Type (Host Name)
+        0xde, 0xad, // Host Name
+        0xbe, 0xef,
+    ];
+    let m = Message::from_bytes(
+        &input,
+        ValidateReserved::No,
+        ValidateVersion::No,
+        ValidateUnused::Yes,
+    );
+    assert_eq!(
+        m,
+        Ok(Message::Control(ControlMessage {
+            length: 30,
+            tunnel_id: 2,
+            session_id: 3,
+            ns: 4,
+            nr: 5,
+            avps: vec![
+                AVP::MessageType(types::MessageType::StartControlConnectionRequest),
+                AVP::HostName(types::HostName {
+                    data: vec![0xde, 0xad, 0xbe, 0xef]
+                })
+            ],
+        }))
+    );
+}
