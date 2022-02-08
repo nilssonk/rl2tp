23d10f24019b3f126f202898a70fcb68d5ebfb74
Kim Nilsson <kim@wayoftao.net>
2022-02-08 22:59:24 +0100

    Add and implement Challenge AVP

--- src/message/tests/valid_avp.rs
+++ src/message/tests/valid_avp.rs
@@ -568,3 +568,50 @@ fn receive_window_size() {
         }))
     );
 }
+
+#[test]
+fn challenge() {
+    // ControlMessage with Challenge AVP
+    use crate::avp::{types, AVP};
+    let input = vec![
+        0x13, 0x00, // Flags
+        0x00, 0x1e, // Length
+        0x00, 0x02, // Tunnel ID
+        0x00, 0x03, // Session ID
+        0x00, 0x04, // Ns
+        0x00, 0x05, // Nr
+        // AVP Payload
+        0x00, 0x08, // Flags and Length
+        0x00, 0x00, // Vendor ID
+        0x00, 0x00, // Attribute Type (Message Type)
+        0x00, 0x01, // Type 1 (StartControlConnectionRequest)
+        // AVP Payload
+        0x00, 0x0a, // Flags and Length
+        0x00, 0x00, // Vendor ID
+        0x00, 0x0b, // Attribute Type (Challenge)
+        0xde, 0xad, // Challenge
+        0xbe, 0xef,
+    ];
+    let m = Message::from_bytes(
+        &input,
+        ValidateReserved::No,
+        ValidateVersion::No,
+        ValidateUnused::Yes,
+    );
+    assert_eq!(
+        m,
+        Ok(Message::Control(ControlMessage {
+            length: 30,
+            tunnel_id: 2,
+            session_id: 3,
+            ns: 4,
+            nr: 5,
+            avps: vec![
+                AVP::MessageType(types::MessageType::StartControlConnectionRequest),
+                AVP::Challenge(types::Challenge {
+                    data: vec![0xde, 0xad, 0xbe, 0xef]
+                })
+            ],
+        }))
+    );
+}
