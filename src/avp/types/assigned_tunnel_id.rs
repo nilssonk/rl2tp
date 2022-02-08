393462ad9210bb240d9b019083d2cb9dcc1daa9b
Kim Nilsson <kim@wayoftao.net>
2022-02-08 22:50:31 +0100

    Add and implement AssignedTunnelId AVP

---
+++ src/avp/types/assigned_tunnel_id.rs
@@ -0,0 +1,23 @@
+use crate::common::{read_u16_be_unchecked, ResultStr};
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct AssignedTunnelId {
+    pub value: u16,
+}
+
+impl AssignedTunnelId {
+    pub fn from(input: &[u8]) -> ResultStr<Self> {
+        if input.len() < 2 {
+            return Err("Incomplete AssignedTunnelId AVP encountered");
+        }
+
+        let value = unsafe { read_u16_be_unchecked(input) };
+        Ok(Self { value })
+    }
+}
+
+impl From<u16> for AssignedTunnelId {
+    fn from(value: u16) -> Self {
+        Self { value }
+    }
+}
--- src/message/tests/valid_avp.rs
+++ src/message/tests/valid_avp.rs
@@ -480,3 +480,47 @@ fn vendor_name() {
         }))
     );
 }
+
+#[test]
+fn assigned_tunnel_id() {
+    // ControlMessage with Assigned Tunnel ID AVP
+    use crate::avp::{types, AVP};
+    let input = vec![
+        0x13, 0x00, // Flags
+        0x00, 0x1c, // Length
+        0x00, 0x02, // Tunnel ID
+        0x00, 0x03, // Session ID
+        0x00, 0x04, // Ns
+        0x00, 0x05, // Nr
+        // AVP Payload
+        0x00, 0x08, // Flags and Length
+        0x00, 0x00, // Vendor ID
+        0x00, 0x00, // Attribute Type (Message Type)
+        0x00, 0x01, // Type 1 (StartControlConnectionRequest)
+        // AVP Payload
+        0x00, 0x08, // Flags and Length
+        0x00, 0x00, // Vendor ID
+        0x00, 0x09, // Attribute Type (Assigned Tunnel ID)
+        0xde, 0xad, // Assigned Tunnel ID
+    ];
+    let m = Message::from_bytes(
+        &input,
+        ValidateReserved::No,
+        ValidateVersion::No,
+        ValidateUnused::Yes,
+    );
+    assert_eq!(
+        m,
+        Ok(Message::Control(ControlMessage {
+            length: 28,
+            tunnel_id: 2,
+            session_id: 3,
+            ns: 4,
+            nr: 5,
+            avps: vec![
+                AVP::MessageType(types::MessageType::StartControlConnectionRequest),
+                AVP::AssignedTunnelId(types::AssignedTunnelId { value: 0xdead })
+            ],
+        }))
+    );
+}
