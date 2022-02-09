4c80fe13f61f1135cbaee3a6def59ed64b16ca70
Kim Nilsson <92694405+nilssonk@users.noreply.github.com>
2022-02-09 22:20:56 +0100

    Add read_u64_be_unchecked (#29)

--- src/common/read.rs
+++ src/common/read.rs
@@ -5,3 +5,8 @@ pub unsafe fn read_u16_be_unchecked(input: &[u8]) -> u16 {
 pub unsafe fn read_u32_be_unchecked(input: &[u8]) -> u32 {
     u32::from_be_bytes(input[..4].try_into().unwrap_unchecked())
 }
+
+#[allow(dead_code)] // Remove upon first use
+pub unsafe fn read_u64_be_unchecked(input: &[u8]) -> u64 {
+    u64::from_be_bytes(input[..8].try_into().unwrap_unchecked())
+}
