e976f40f560938528d93fe0169a963588afb55da
Kim Nilsson <92694405+nilssonk@users.noreply.github.com>
2022-02-10 13:49:36 +0100

    Add and implement TieBreaker AVP (#30)

--- src/common/read.rs
+++ src/common/read.rs
@@ -6,7 +6,6 @@ pub unsafe fn read_u32_be_unchecked(input: &[u8]) -> u32 {
     u32::from_be_bytes(input[..4].try_into().unwrap_unchecked())
 }
 
-#[allow(dead_code)] // Remove upon first use
 pub unsafe fn read_u64_be_unchecked(input: &[u8]) -> u64 {
     u64::from_be_bytes(input[..8].try_into().unwrap_unchecked())
 }
