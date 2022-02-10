359de97a709fbb33185cfdbc1f56fac5b3c94566
Kim Nilsson <92694405+nilssonk@users.noreply.github.com>
2022-02-10 13:53:31 +0100

    Add and implement HostName AVP (#32)

---
+++ src/avp/types/host_name.rs
@@ -0,0 +1,18 @@
+use crate::common::ResultStr;
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct HostName {
+    pub data: Vec<u8>,
+}
+
+impl HostName {
+    pub fn from(input: &[u8]) -> ResultStr<Self> {
+        if input.is_empty() {
+            return Err("Incomplete HostName AVP encountered");
+        }
+
+        Ok(Self {
+            data: input.to_owned(),
+        })
+    }
+}
