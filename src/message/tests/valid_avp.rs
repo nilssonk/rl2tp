66c8a02c3105f3ef57004e0e959ac26afe1b89ca
Kim Nilsson <kim@wayoftao.net>
2022-02-12 23:00:41 +0100

    Add and implement Private Group ID AVP

--- src/message/tests/valid_avp.rs
+++ src/message/tests/valid_avp.rs
@@ -1279,3 +1279,49 @@ fn physical_channel_id() {
         }))
     );
 }
+
+#[test]
+fn private_group_id() {
+    // ControlMessage with Private Group ID AVP
+    use crate::avp::{types, AVP};
+    let input = vec![
+        0x13, 0x00, // Flags
+        0x00, 0x1e, // Length
+        0x00, 0x02, // Tunnel ID
+        0x00, 0x03, // Session ID
+        0x00, 0x04, // Ns
+        0x00, 0x05, // Nr
+        // AVP Payload
+        0x00, 0x08, // Flags and Length
+        0x00, 0x00, // Vendor ID
+        0x00, 0x00, // Attribute Type (Message Type)
+        0x00, 0x0c, // Type 12 (IncomingCallConnected)
+        // AVP Payload
+        0x00, 0x0a, // Flags and Length
+        0x00, 0x00, // Vendor ID
+        0x00, 0x25, // Attribute Type (Private Group ID)
+        0xde, 0xad, 0xbe, 0xef, // Private Group ID
+    ];
+    let m = Message::from_bytes(
+        &input,
+        ValidateReserved::No,
+        ValidateVersion::No,
+        ValidateUnused::Yes,
+    );
+    assert_eq!(
+        m,
+        Ok(Message::Control(ControlMessage {
+            length: 30,
+            tunnel_id: 2,
+            session_id: 3,
+            ns: 4,
+            nr: 5,
+            avps: vec![
+                AVP::MessageType(types::MessageType::IncomingCallConnected),
+                AVP::PrivateGroupId(types::PrivateGroupId {
+                    data: vec![0xde, 0xad, 0xbe, 0xef]
+                })
+            ],
+        }))
+    );
+}
