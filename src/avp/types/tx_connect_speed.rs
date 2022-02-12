3fb4a5279cd918004f1a51303721b8e86e28d198
Kim Nilsson <kim@wayoftao.net>
2022-02-12 22:40:14 +0100

    Add and implement Tx Connect Speed AVP

---
+++ src/avp/types/tx_connect_speed.rs
@@ -0,0 +1,23 @@
+use crate::common::{read_u32_be_unchecked, ResultStr};
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct TxConnectSpeed {
+    pub value: u32,
+}
+
+impl TxConnectSpeed {
+    pub fn from(input: &[u8]) -> ResultStr<Self> {
+        if input.len() < 4 {
+            return Err("Incomplete TxConnectSpeed AVP encountered");
+        }
+
+        let value = unsafe { read_u32_be_unchecked(input) };
+        Ok(Self { value })
+    }
+}
+
+impl From<u32> for TxConnectSpeed {
+    fn from(value: u32) -> Self {
+        Self { value }
+    }
+}
