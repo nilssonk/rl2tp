b79fbd86fc2233888a174704b9748f0478850f68
Kim Nilsson <kim@wayoftao.net>
2022-02-12 23:21:22 +0100

    Add and implement Last Received LCP CONFREQ AVP

--- src/message/tests/valid_avp.rs
+++ src/message/tests/valid_avp.rs
@@ -1460,3 +1460,49 @@ fn last_sent_lcp_conf_req() {
         }))
     );
 }
+
+#[test]
+fn last_received_lcp_conf_req() {
+    // ControlMessage with Last Received LCP CONFREQ AVP
+    use crate::avp::{types, AVP};
+    let input = vec![
+        0x13, 0x00, // Flags
+        0x00, 0x1e, // Length
+        0x00, 0x02, // Tunnel ID
+        0x00, 0x03, // Session ID
+        0x00, 0x04, // Ns
+        0x00, 0x05, // Nr
+        // AVP Payload
+        0x00, 0x08, // Flags and Length
+        0x00, 0x00, // Vendor ID
+        0x00, 0x00, // Attribute Type (Message Type)
+        0x00, 0x0c, // Type 12 (IncomingCallConnected)
+        // AVP Payload
+        0x00, 0x0a, // Flags and Length
+        0x00, 0x00, // Vendor ID
+        0x00, 0x1c, // Attribute Type (Last Received LCP CONFREQ)
+        0xde, 0xad, 0xbe, 0xef, // Last Received LCP CONFREQ
+    ];
+    let m = Message::from_bytes(
+        &input,
+        ValidateReserved::No,
+        ValidateVersion::No,
+        ValidateUnused::Yes,
+    );
+    assert_eq!(
+        m,
+        Ok(Message::Control(ControlMessage {
+            length: 30,
+            tunnel_id: 2,
+            session_id: 3,
+            ns: 4,
+            nr: 5,
+            avps: vec![
+                AVP::MessageType(types::MessageType::IncomingCallConnected),
+                AVP::LastReceivedLcpConfReq(types::LastReceivedLcpConfReq {
+                    data: vec![0xde, 0xad, 0xbe, 0xef]
+                })
+            ],
+        }))
+    );
+}
