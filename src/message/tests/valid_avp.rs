ff7f9285a6f4df2d6b76339efe818f162b0a2fd0
Kim Nilsson <92694405+nilssonk@users.noreply.github.com>
2022-02-14 20:59:17 +0100

    Add and implement Bearer Type AVP (#44)

--- src/message/tests/valid_avp.rs
+++ src/message/tests/valid_avp.rs
@@ -891,3 +891,61 @@ fn maximum_bps() {
         }))
     );
 }
+
+#[test]
+fn bearer_type() {
+    // ControlMessage with Bearer Type AVP
+    use crate::avp::{types, AVP};
+    let input = vec![
+        0x13, 0x00, // Flags
+        0x00, 0x1e, // Length
+        0x00, 0x02, // Tunnel ID
+        0x00, 0x03, // Session ID
+        0x00, 0x04, // Ns
+        0x00, 0x05, // Nr
+        // AVP Payload
+        0x00, 0x08, // Flags and Length
+        0x00, 0x00, // Vendor ID
+        0x00, 0x00, // Attribute Type (Message Type)
+        0x00, 0x07, // Type 7 (OutgoingCallRequest)
+        // AVP Payload
+        0x00, 0x0a, // Flags and Length
+        0x00, 0x00, // Vendor ID
+        0x00, 0x12, // Attribute Type (Bearer Type)
+        0x00, 0x00, 0x00, 0xc0, // Digital and analog access requested
+    ];
+    let m = Message::from_bytes(
+        &input,
+        ValidateReserved::No,
+        ValidateVersion::No,
+        ValidateUnused::Yes,
+    );
+    assert_eq!(
+        m,
+        Ok(Message::Control(ControlMessage {
+            length: 30,
+            tunnel_id: 2,
+            session_id: 3,
+            ns: 4,
+            nr: 5,
+            avps: vec![
+                AVP::MessageType(types::MessageType::OutgoingCallRequest),
+                AVP::BearerType(types::BearerType::from_raw(0x000000c0))
+            ],
+        }))
+    );
+
+    match m {
+        Ok(Message::Control(real_m)) => {
+            let avp = &real_m.avps[1];
+            match avp {
+                AVP::BearerType(bearer) => {
+                    assert!(bearer.is_analog_request());
+                    assert!(bearer.is_digital_request());
+                }
+                _ => panic!(),
+            }
+        }
+        _ => panic!(),
+    }
+}
