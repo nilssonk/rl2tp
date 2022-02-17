98d5d0530ed3e87fc52a92d25a3fbd6965bf7d14
Kim Nilsson <92694405+nilssonk@users.noreply.github.com>
2022-02-17 19:05:08 +0100

    Add and implement Rx Connect Speed AVP (#50)

---
+++ src/avp/types/rx_connect_speed.rs
@@ -0,0 +1,23 @@
+use crate::common::{read_u32_be_unchecked, ResultStr};
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct RxConnectSpeed {
+    pub value: u32,
+}
+
+impl RxConnectSpeed {
+    pub fn from(input: &[u8]) -> ResultStr<Self> {
+        if input.len() < 4 {
+            return Err("Incomplete RxConnectSpeed AVP encountered");
+        }
+
+        let value = unsafe { read_u32_be_unchecked(input) };
+        Ok(Self { value })
+    }
+}
+
+impl From<u32> for RxConnectSpeed {
+    fn from(value: u32) -> Self {
+        Self { value }
+    }
+}
