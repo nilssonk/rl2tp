3ef15b2b5472a90ba892d4561507964834366771
Kim Nilsson <kim@wayoftao.net>
2022-02-25 23:21:44 +0100

    Add Accm AVP writing
    * Make macro for write_read tests

--- src/avp/tests/write_read.rs
+++ src/avp/tests/write_read.rs
@@ -1,15 +1,32 @@
 use crate::avp::*;
-use crate::common::VecWriter;
+use crate::common::{SliceReader, VecWriter};
 
-#[test]
-fn message_type() {
-    let input = AVP::MessageType(types::MessageType::IncomingCallConnected);
-    let mut w = VecWriter::new();
-    unsafe {
-        input.write(&mut w);
+macro_rules! io_tests {
+    [$($name:ident => $input:expr),+] => {
+        $(
+        #[test]
+        fn $name() {
+            // Serialize input
+            let input = $input;
+            let mut w = VecWriter::new();
+            unsafe { $input.write(&mut w) };
+
+            // Deserialize to output
+            let r = Box::new(SliceReader::from(&w.data));
+            let avps = AVP::try_read_greedy(r);
+
+            // Select first and only AVP, assert successful deserialization
+            let output = avps.into_iter().next().unwrap().unwrap();
+
+            assert_eq!(input, output);
+        }
+    )+
     }
-    let r = Box::new(SliceReader::from(&w.data));
-    let avps = AVP::try_read_greedy(r);
-    let output = avps.into_iter().next().unwrap().unwrap();
-    assert_eq!(input, output);
 }
+
+io_tests![
+message_type => AVP::MessageType(types::MessageType::IncomingCallConnected),
+accm => AVP::Accm(types::Accm {
+    send_accm: [0x01, 0x02, 0x03, 0x04],
+    receive_accm: [0x05, 0x06, 0x07, 0x08],
+})];
