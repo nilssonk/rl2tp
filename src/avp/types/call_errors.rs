afae4f392f836ff27f6ba56c9d98c75f0ada29df
Kim Nilsson <92694405+nilssonk@users.noreply.github.com>
2022-02-26 13:36:49 +0100

    Add and implement Call Errors AVP (#62)

---
+++ src/avp/types/call_errors.rs
@@ -0,0 +1,43 @@
+use crate::common::{read_u32_be_unchecked, ResultStr};
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct CallErrors {
+    pub crc_errors: u32,
+    pub framing_errors: u32,
+    pub hardware_overruns: u32,
+    pub buffer_overruns: u32,
+    pub timeout_errors: u32,
+    pub alignment_errors: u32,
+}
+
+impl CallErrors {
+    pub fn from(input: &[u8]) -> ResultStr<Self> {
+        if input.len() < 26 {
+            return Err("Incomplete CallErrors AVP encountered");
+        }
+
+        // Skip reserved
+        let mut offset = 2;
+
+        let crc_errors = unsafe { read_u32_be_unchecked(&input[offset..]) };
+        offset += 4;
+        let framing_errors = unsafe { read_u32_be_unchecked(&input[offset..]) };
+        offset += 4;
+        let hardware_overruns = unsafe { read_u32_be_unchecked(&input[offset..]) };
+        offset += 4;
+        let buffer_overruns = unsafe { read_u32_be_unchecked(&input[offset..]) };
+        offset += 4;
+        let timeout_errors = unsafe { read_u32_be_unchecked(&input[offset..]) };
+        offset += 4;
+        let alignment_errors = unsafe { read_u32_be_unchecked(&input[offset..]) };
+
+        Ok(Self {
+            crc_errors,
+            framing_errors,
+            hardware_overruns,
+            buffer_overruns,
+            timeout_errors,
+            alignment_errors,
+        })
+    }
+}
