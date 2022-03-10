f2701ae06d07c7398abca92ec147504c83c3847e
Kim Nilsson <92694405+nilssonk@users.noreply.github.com>
2022-03-10 19:13:57 +0100

    Add VecWriter impl of Writer (#69)

--- src/common.rs
+++ src/common.rs
@@ -1,9 +1,14 @@
 mod reader;
-mod result_str;
-mod slice_reader;
-mod writer;
-
 pub use reader::*;
+
+mod result_str;
 pub use result_str::*;
+
+mod slice_reader;
 pub use slice_reader::*;
+
+mod vec_writer;
+pub use vec_writer::*;
+
+mod writer;
 pub use writer::*;
---
+++ src/common/vec_writer.rs
@@ -0,0 +1,45 @@
+#[cfg(test)]
+mod tests;
+
+use crate::common::Writer;
+
+#[derive(Clone, Debug, Default, PartialEq)]
+pub struct VecWriter {
+    pub data: Vec<u8>,
+}
+
+impl VecWriter {
+    #[allow(dead_code)] // Remove on first use
+    pub fn new() -> Self {
+        Default::default()
+    }
+}
+
+impl Writer for VecWriter {
+    fn len(&self) -> usize {
+        self.data.len()
+    }
+
+    unsafe fn write_bytes_unchecked(&mut self, bytes: &[u8]) {
+        self.data.extend_from_slice(bytes);
+    }
+
+    unsafe fn write_u8_unchecked(&mut self, value: u8) {
+        self.data.push(value);
+    }
+
+    unsafe fn write_u16_be_unchecked(&mut self, value: u16) {
+        let bytes = value.to_be_bytes();
+        self.data.extend_from_slice(&bytes);
+    }
+
+    unsafe fn write_u32_be_unchecked(&mut self, value: u32) {
+        let bytes = value.to_be_bytes();
+        self.data.extend_from_slice(&bytes);
+    }
+
+    unsafe fn write_u64_be_unchecked(&mut self, value: u64) {
+        let bytes = value.to_be_bytes();
+        self.data.extend_from_slice(&bytes);
+    }
+}
