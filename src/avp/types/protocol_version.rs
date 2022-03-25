f7e760427d5bd0d0a53fc740a48e11fce7a9cc6d
Kim Nilsson <kim@wayoftao.net>
2022-03-25 22:54:07 +0100

    Implement QueryableAVP for ProtocolVersion

--- src/avp/types/protocol_version.rs
+++ src/avp/types/protocol_version.rs
@@ -8,8 +8,10 @@ pub struct ProtocolVersion {
 }
 
 impl ProtocolVersion {
+    const LENGTH: u16 = 2;
+
     pub fn try_read<'a>(mut reader: Box<dyn Reader<'a> + 'a>) -> ResultStr<Self> {
-        if reader.len() < 2 {
+        if reader.len() < Self::LENGTH as usize {
             return Err("Incomplete ProtocolVersion AVP encountered");
         }
 
@@ -21,6 +23,6 @@ impl ProtocolVersion {
 
 impl QueryableAVP for ProtocolVersion {
     fn get_length(&self) -> u16 {
-        unimplemented!();
+        Self::LENGTH
     }
 }
