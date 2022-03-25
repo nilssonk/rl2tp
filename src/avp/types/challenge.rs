c1f4f918c806bdb875a2ec41f3c46115e19114e4
Kim Nilsson <kim@wayoftao.net>
2022-03-25 22:23:20 +0100

    Implement QueryableAVP for Challenge

--- src/avp/types/challenge.rs
+++ src/avp/types/challenge.rs
@@ -20,6 +20,8 @@ impl Challenge {
 
 impl QueryableAVP for Challenge {
     fn get_length(&self) -> u16 {
-        unimplemented!();
+        assert!(self.data.len() <= u16::MAX as usize);
+
+        self.data.len() as u16
     }
 }
