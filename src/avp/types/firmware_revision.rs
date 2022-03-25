4ebbc4a3bc0027889cd3615f9b482638fdee93f0
Kim Nilsson <kim@wayoftao.net>
2022-03-25 22:24:03 +0100

    Implement QueryableAVP for FirmwareRevision

--- src/avp/types/firmware_revision.rs
+++ src/avp/types/firmware_revision.rs
@@ -7,8 +7,10 @@ pub struct FirmwareRevision {
 }
 
 impl FirmwareRevision {
+    const LENGTH: u16 = 2;
+
     pub fn try_read<'a>(mut reader: Box<dyn Reader<'a> + 'a>) -> ResultStr<Self> {
-        if reader.len() < 2 {
+        if reader.len() < Self::LENGTH as usize {
             return Err("Incomplete FirmwareRevision AVP encountered");
         }
 
@@ -25,6 +27,6 @@ impl From<u16> for FirmwareRevision {
 
 impl QueryableAVP for FirmwareRevision {
     fn get_length(&self) -> u16 {
-        unimplemented!();
+        Self::LENGTH
     }
 }
