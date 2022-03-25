19969d064ee44183fea2b70507d2b598446ef5e6
Kim Nilsson <kim@wayoftao.net>
2022-03-25 23:05:50 +0100

    Implement QueryableAVP for RandomVector

--- src/avp/types/random_vector.rs
+++ src/avp/types/random_vector.rs
@@ -1,14 +1,18 @@
 use crate::avp::QueryableAVP;
 use crate::common::{Reader, ResultStr};
 
+const G_LENGTH: u16 = 4;
+
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct RandomVector {
-    pub data: [u8; 4],
+    pub data: [u8; G_LENGTH as usize],
 }
 
 impl RandomVector {
+    const LENGTH: u16 = G_LENGTH;
+
     pub fn try_read<'a>(reader: Box<dyn Reader<'a> + 'a>) -> ResultStr<Self> {
-        if reader.len() < 4 {
+        if reader.len() < Self::LENGTH as usize {
             return Err("Incomplete Random Vector AVP payload encountered");
         }
 
@@ -20,6 +24,6 @@ impl RandomVector {
 
 impl QueryableAVP for RandomVector {
     fn get_length(&self) -> u16 {
-        unimplemented!();
+        Self::LENGTH
     }
 }
