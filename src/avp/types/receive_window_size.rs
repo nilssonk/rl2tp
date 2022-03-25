8288a6fd419f1def93fbdf78ec9bacb8c157df81
Kim Nilsson <kim@wayoftao.net>
2022-03-25 23:06:30 +0100

    Implement QueryableAVP for ReceiveWindowSize

--- src/avp/types/receive_window_size.rs
+++ src/avp/types/receive_window_size.rs
@@ -7,8 +7,10 @@ pub struct ReceiveWindowSize {
 }
 
 impl ReceiveWindowSize {
+    const LENGTH: u16 = 2;
+
     pub fn try_read<'a>(mut reader: Box<dyn Reader<'a> + 'a>) -> ResultStr<Self> {
-        if reader.len() < 2 {
+        if reader.len() < Self::LENGTH as usize {
             return Err("Incomplete ReceiveWindowSize AVP encountered");
         }
 
@@ -25,6 +27,6 @@ impl From<u16> for ReceiveWindowSize {
 
 impl QueryableAVP for ReceiveWindowSize {
     fn get_length(&self) -> u16 {
-        unimplemented!();
+        Self::LENGTH
     }
 }
