91268c211ea99ee19d26a62880f5b6fe42345146
Kim Nilsson <kim@wayoftao.net>
2022-03-25 22:52:07 +0100

    Implement QueryableAVP for PhysicalChannelId

--- src/avp/types/physical_channel_id.rs
+++ src/avp/types/physical_channel_id.rs
@@ -1,25 +1,34 @@
 use crate::avp::QueryableAVP;
 use crate::common::{Reader, ResultStr};
 
+const G_LENGTH: u16 = 4;
+
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct PhysicalChannelId {
-    pub data: [u8; 4],
+    pub data: [u8; G_LENGTH as usize],
 }
 
 impl PhysicalChannelId {
+    const LENGTH: u16 = G_LENGTH;
+
     pub fn try_read<'a>(reader: Box<dyn Reader<'a> + 'a>) -> ResultStr<Self> {
-        if reader.len() < 4 {
+        if reader.len() < Self::LENGTH as usize {
             return Err("Incomplete PhysicalChannelId payload encountered");
         }
 
         Ok(Self {
-            data: unsafe { reader.peek_bytes(4)?.try_into().unwrap_unchecked() },
+            data: unsafe {
+                reader
+                    .peek_bytes(Self::LENGTH as usize)?
+                    .try_into()
+                    .unwrap_unchecked()
+            },
         })
     }
 }
 
 impl QueryableAVP for PhysicalChannelId {
     fn get_length(&self) -> u16 {
-        unimplemented!();
+        Self::LENGTH
     }
 }
