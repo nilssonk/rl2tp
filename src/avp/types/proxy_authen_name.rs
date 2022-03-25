420fbf53cdb66d7aab7433e853f76c0cb97effbe
Kim Nilsson <kim@wayoftao.net>
2022-03-25 22:56:31 +0100

    Implement QueryableAVP for ProxyAuthenName

--- src/avp/types/proxy_authen_name.rs
+++ src/avp/types/proxy_authen_name.rs
@@ -20,6 +20,8 @@ impl ProxyAuthenName {
 
 impl QueryableAVP for ProxyAuthenName {
     fn get_length(&self) -> u16 {
-        unimplemented!();
+        assert!(self.data.len() <= u16::MAX as usize);
+
+        self.data.len() as u16
     }
 }
