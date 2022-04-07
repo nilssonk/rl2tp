9345f16ea0589a7f8ca7e52859b3990bb4bd35be
Kim Nilsson <92694405+nilssonk@users.noreply.github.com>
2022-04-07 23:25:58 +0200

    Implement QueryableAVP for LastReceivedLcpConfReq (#95)

--- src/avp/types/last_received_lcp_conf_req.rs
+++ src/avp/types/last_received_lcp_conf_req.rs
@@ -20,6 +20,8 @@ impl LastReceivedLcpConfReq {
 
 impl QueryableAVP for LastReceivedLcpConfReq {
     fn get_length(&self) -> u16 {
-        unimplemented!();
+        assert!(self.data.len() <= u16::MAX as usize);
+
+        self.data.len() as u16
     }
 }
