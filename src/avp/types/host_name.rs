f8319c86c0051ce2c8fc68dfb40b065928568717
Kim Nilsson <92694405+nilssonk@users.noreply.github.com>
2022-04-07 23:25:03 +0200

    Implement QueryableAVP for HostName (#97)

--- src/avp/types/host_name.rs
+++ src/avp/types/host_name.rs
@@ -20,6 +20,8 @@ impl HostName {
 
 impl QueryableAVP for HostName {
     fn get_length(&self) -> u16 {
-        unimplemented!();
+        assert!(self.data.len() <= u16::MAX as usize);
+
+        self.data.len() as u16
     }
 }
