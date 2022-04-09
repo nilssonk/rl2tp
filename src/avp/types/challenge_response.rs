5510dc3783cfb3e82f60485c702e695bc72434fb
Kim Nilsson <kim@wayoftao.net>
2022-04-09 15:55:52 +0200

    Add ChallengeResponse AVP writing

--- src/avp/types/challenge_response.rs
+++ src/avp/types/challenge_response.rs
@@ -1,12 +1,13 @@
+use crate::avp::header::Header;
 use crate::avp::{QueryableAVP, WritableAVP};
 use crate::common::{Reader, ResultStr, Writer};
-
 #[derive(Clone, Debug, PartialEq)]
 pub struct ChallengeResponse {
     pub data: [u8; 16],
 }
 
 impl ChallengeResponse {
+    const ATTRIBUTE_TYPE: u16 = 13;
     const LENGTH: u16 = 16;
 
     pub fn try_read<'a>(reader: Box<dyn Reader<'a> + 'a>) -> ResultStr<Self> {
@@ -22,12 +23,16 @@ impl ChallengeResponse {
 
 impl QueryableAVP for ChallengeResponse {
     fn get_length(&self) -> u16 {
-        Self::LENGTH
+        Header::LENGTH + Self::LENGTH
     }
 }
 
 impl WritableAVP for ChallengeResponse {
-    unsafe fn write(&self, _writer: &mut dyn Writer) {
-        unimplemented!();
+    unsafe fn write(&self, writer: &mut dyn Writer) {
+        let header =
+            Header::with_payload_length_and_attribute_type(Self::LENGTH, Self::ATTRIBUTE_TYPE);
+        header.write(writer);
+
+        writer.write_bytes_unchecked(&self.data);
     }
 }
