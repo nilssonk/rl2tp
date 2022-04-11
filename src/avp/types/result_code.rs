dde87e3be0e18c85aee88dfecfd7ec916aea364e
Kim Nilsson <92694405+nilssonk@users.noreply.github.com>
2022-04-11 23:27:22 +0200

    Implement QueryableAVP for ResultCode (#113)

--- src/avp/types/result_code.rs
+++ src/avp/types/result_code.rs
@@ -15,8 +15,11 @@ pub struct ResultCode {
 }
 
 impl ResultCode {
+    const FIXED_LENGTH: u16 = 2;
+    const ERROR_LENGTH: u16 = 2;
+
     pub fn try_read<'a>(mut reader: Box<dyn Reader<'a> + 'a>) -> ResultStr<Self> {
-        if reader.len() < 2 {
+        if reader.len() < Self::FIXED_LENGTH as usize {
             return Err("Incomplete ResultCode AVP payload encountered");
         }
 
@@ -25,14 +28,14 @@ impl ResultCode {
 
         let mut maybe_error: Option<Error> = None;
         let mut maybe_error_message: Option<String> = None;
-        if reader.len() >= 4 {
+        if reader.len() >= Self::ERROR_LENGTH as usize {
             let error_raw = unsafe { reader.read_u16_be_unchecked() };
             maybe_error = error_raw.try_into().ok();
             if maybe_error.is_none() {
                 return Err("Invalid ResultCode error encountered");
             }
 
-            if reader.len() > 4 {
+            if !reader.is_empty() {
                 maybe_error_message = match std::str::from_utf8(reader.peek_bytes(reader.len())?) {
                     Ok(s) => Some(s.to_owned()),
                     Err(_) => return Err("Invalid ResultCode error message encountered"),
@@ -50,6 +53,18 @@ impl ResultCode {
 
 impl QueryableAVP for ResultCode {
     fn get_length(&self) -> u16 {
-        unimplemented!();
+        let mut length = Self::FIXED_LENGTH;
+
+        if self.error.is_some() {
+            length += Self::ERROR_LENGTH;
+
+            if let Some(value) = &self.error_message {
+                assert!(value.len() <= (u16::MAX - length) as usize);
+
+                length += value.len() as u16;
+            }
+        }
+
+        length
     }
 }
