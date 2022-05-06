4ec40a7aee914452c602696c908e3fc06fe61a5b
Kim Nilsson <kim@wayoftao.net>
2022-05-06 23:44:03 +0200

    Add ProxyAuthenName AVP writing

--- src/avp/types/proxy_authen_name.rs
+++ src/avp/types/proxy_authen_name.rs
@@ -3,30 +3,33 @@ use crate::common::{Reader, ResultStr, Writer};
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct ProxyAuthenName {
-    pub data: Vec<u8>,
+    pub value: Vec<u8>,
 }
 
 impl ProxyAuthenName {
+    const ATTRIBUTE_TYPE: u16 = 30;
+
     pub fn try_read<'a, 'b>(reader: &'b mut impl Reader<'a>) -> ResultStr<Self> {
         if reader.is_empty() {
             return Err("Incomplete ProxyAuthenName AVP encountered");
         }
 
         Ok(Self {
-            data: reader.read_bytes(reader.len())?,
+            value: reader.read_bytes(reader.len())?,
         })
     }
 }
 
 impl QueryableAVP for ProxyAuthenName {
     fn get_length(&self) -> usize {
-        self.data.len()
+        self.value.len()
     }
 }
 
 impl WritableAVP for ProxyAuthenName {
     #[inline]
-    unsafe fn write(&self, _writer: &mut impl Writer) {
-        unimplemented!();
+    unsafe fn write(&self, writer: &mut impl Writer) {
+        writer.write_u16_be_unchecked(Self::ATTRIBUTE_TYPE);
+        writer.write_bytes_unchecked(&self.value);
     }
 }
