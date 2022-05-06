e58325d0ce5e5c461a19cd9e75c4add2a2cf0da4
Kim Nilsson <92694405+nilssonk@users.noreply.github.com>
2022-05-06 22:02:33 +0200

    Add FramingCapabilities AVP writing (#151)

--- src/avp/types/framing_capabilities.rs
+++ src/avp/types/framing_capabilities.rs
@@ -7,10 +7,15 @@ pub struct FramingCapabilities {
 }
 
 impl FramingCapabilities {
+    const ATTRIBUTE_TYPE: u16 = 3;
     const LENGTH: u16 = 4;
 
-    pub fn from_raw(data: u32) -> Self {
-        Self { data }
+    pub fn new(async_framing_supported: bool, sync_framing_supported: bool) -> Self {
+        let async_bit = (async_framing_supported as u32) << 6;
+        let sync_bit = (sync_framing_supported as u32) << 7;
+        Self {
+            data: async_bit | sync_bit,
+        }
     }
 
     pub fn try_read<'a, 'b>(reader: &'b mut impl Reader<'a>) -> ResultStr<Self> {
@@ -19,7 +24,7 @@ impl FramingCapabilities {
         }
 
         let data = unsafe { reader.read_u32_be_unchecked() };
-        Ok(Self::from_raw(data))
+        Ok(Self { data })
     }
 
     pub fn is_async_framing_supported(&self) -> bool {
@@ -39,7 +44,8 @@ impl QueryableAVP for FramingCapabilities {
 
 impl WritableAVP for FramingCapabilities {
     #[inline]
-    unsafe fn write(&self, _writer: &mut impl Writer) {
-        unimplemented!();
+    unsafe fn write(&self, writer: &mut impl Writer) {
+        writer.write_u16_be_unchecked(Self::ATTRIBUTE_TYPE);
+        writer.write_u32_be_unchecked(self.data);
     }
 }
