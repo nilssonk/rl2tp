8069011c937c7641c65f14998a8d255c067811e6
Kim Nilsson <kim@wayoftao.net>
2022-05-06 22:14:19 +0200

    Add LastReceivedLcpConfReq AVP writing

--- src/avp/types/last_received_lcp_conf_req.rs
+++ src/avp/types/last_received_lcp_conf_req.rs
@@ -3,30 +3,34 @@ use crate::common::{Reader, ResultStr, Writer};
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct LastReceivedLcpConfReq {
-    pub data: Vec<u8>,
+    pub value: Vec<u8>,
 }
 
 impl LastReceivedLcpConfReq {
+    const ATTRIBUTE_TYPE: u16 = 28;
+
     pub fn try_read<'a, 'b>(reader: &'b mut impl Reader<'a>) -> ResultStr<Self> {
         if reader.is_empty() {
             return Err("Incomplete LastReceivedLcpConfReq AVP encountered");
         }
 
         Ok(Self {
-            data: reader.read_bytes(reader.len())?,
+            value: reader.read_bytes(reader.len())?,
         })
     }
 }
 
 impl QueryableAVP for LastReceivedLcpConfReq {
     fn get_length(&self) -> u16 {
-        self.data.len() as u16
+        assert!(self.value.len() <= u16::MAX as usize);
+        self.value.len() as u16
     }
 }
 
 impl WritableAVP for LastReceivedLcpConfReq {
     #[inline]
-    unsafe fn write(&self, _writer: &mut impl Writer) {
-        unimplemented!();
+    unsafe fn write(&self, writer: &mut impl Writer) {
+        writer.write_u16_be_unchecked(Self::ATTRIBUTE_TYPE);
+        writer.write_bytes_unchecked(&self.value);
     }
 }
