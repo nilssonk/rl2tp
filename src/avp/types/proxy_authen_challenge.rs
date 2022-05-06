e412b08fd2302c8346b7d04296a08bccd19821ab
Kim Nilsson <kim@wayoftao.net>
2022-05-06 22:50:23 +0200

    Add ProxyAuthenChallenge AVP writing

--- src/avp/types/proxy_authen_challenge.rs
+++ src/avp/types/proxy_authen_challenge.rs
@@ -3,30 +3,33 @@ use crate::common::{Reader, ResultStr, Writer};
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct ProxyAuthenChallenge {
-    pub data: Vec<u8>,
+    pub value: Vec<u8>,
 }
 
 impl ProxyAuthenChallenge {
+    const ATTRIBUTE_TYPE: u16 = 31;
+
     pub fn try_read<'a, 'b>(reader: &'b mut impl Reader<'a>) -> ResultStr<Self> {
         if reader.is_empty() {
             return Err("Incomplete ProxyAuthenChallenge AVP encountered");
         }
 
         Ok(Self {
-            data: reader.read_bytes(reader.len())?,
+            value: reader.read_bytes(reader.len())?,
         })
     }
 }
 
 impl QueryableAVP for ProxyAuthenChallenge {
     fn get_length(&self) -> usize {
-        self.data.len()
+        self.value.len()
     }
 }
 
 impl WritableAVP for ProxyAuthenChallenge {
     #[inline]
-    unsafe fn write(&self, _writer: &mut impl Writer) {
-        unimplemented!();
+    unsafe fn write(&self, writer: &mut impl Writer) {
+        writer.write_u16_be_unchecked(Self::ATTRIBUTE_TYPE);
+        writer.write_bytes_unchecked(&self.value);
     }
 }
