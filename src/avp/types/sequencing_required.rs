d120ff4455f5d3c28f26febb27521b73ef58f637
Kim Nilsson <92694405+nilssonk@users.noreply.github.com>
2022-05-20 21:36:17 +0200

    Add SequencingRequired AVP writing (#173)

--- src/avp/types/sequencing_required.rs
+++ src/avp/types/sequencing_required.rs
@@ -4,6 +4,10 @@ use crate::common::Writer;
 #[derive(Clone, Debug, Default, PartialEq)]
 pub struct SequencingRequired {}
 
+impl SequencingRequired {
+    const ATTRIBUTE_TYPE: u16 = 39;
+}
+
 impl QueryableAVP for SequencingRequired {
     fn get_length(&self) -> usize {
         0
@@ -12,7 +16,7 @@ impl QueryableAVP for SequencingRequired {
 
 impl WritableAVP for SequencingRequired {
     #[inline]
-    unsafe fn write(&self, _writer: &mut impl Writer) {
-        unimplemented!();
+    unsafe fn write(&self, writer: &mut impl Writer) {
+        writer.write_u16_be_unchecked(Self::ATTRIBUTE_TYPE);
     }
 }
