e99dd29ad37f0b3a7019e874195b8ba6d6dd7dcc
Kim Nilsson <92694405+nilssonk@users.noreply.github.com>
2022-05-20 21:43:22 +0200

    Add TxConnectSpeed AVP writing (#176)

--- src/avp/types/tx_connect_speed.rs
+++ src/avp/types/tx_connect_speed.rs
@@ -7,6 +7,7 @@ pub struct TxConnectSpeed {
 }
 
 impl TxConnectSpeed {
+    const ATTRIBUTE_TYPE: u16 = 24;
     const LENGTH: usize = 4;
 
     pub fn try_read<'a, 'b>(reader: &'b mut impl Reader<'a>) -> ResultStr<Self> {
@@ -33,7 +34,8 @@ impl QueryableAVP for TxConnectSpeed {
 
 impl WritableAVP for TxConnectSpeed {
     #[inline]
-    unsafe fn write(&self, _writer: &mut impl Writer) {
-        unimplemented!();
+    unsafe fn write(&self, writer: &mut impl Writer) {
+        writer.write_u16_be_unchecked(Self::ATTRIBUTE_TYPE);
+        writer.write_u32_be_unchecked(self.value);
     }
 }
