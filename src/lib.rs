a9d60e878282790ac55f833d695f644949ea867b
Kim Nilsson <92694405+nilssonk@users.noreply.github.com>
2022-05-22 13:13:18 +0200

    Add crate blurb (#181)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! A Rust implementation of the L2TP protocol.
+//
+//! Provides the full set of protocol messages required to speak L2TP, together
+//! with some utility traits used to read and write them.
+
 #![cfg_attr(feature = "fail-on-warnings", deny(warnings))]
 
 pub mod avp;
